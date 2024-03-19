d15b5e3745916314cafd02b89b0d8b9c996aee74
LeeHickey <lee@hiddenbridge.io>
2024-03-19 15:24:24 +0000

    relocating

--- sim/sim.go
+++ sim/sim.go
@@ -12,20 +12,20 @@ type eventPayload map[string]any
 type event struct {
 	Event     string       `json:"event"`
 	UserID    string       `json:"user_id"`
-	Timestamp time.Time    `json:"timestamp"`
+	Timestamp int64        `json:"timestamp"`
 	Params    eventPayload `json:"params,omitempty"`
 }
 
 type choose_tier struct {
-	name   string
+	name   any
 	weight int
 }
 
-func chooseRand(choices []choose_tier) string {
+func chooseRand(choices []choose_tier) any {
 	return choose(rand.Intn(100), choices)
 }
 
-func choose(weight int, choices []choose_tier) string {
+func choose(weight int, choices []choose_tier) any {
 	total := 0
 	for _, v := range choices {
 		total += v.weight
@@ -58,6 +58,24 @@ var battle_arena = []choose_tier{
 	{"Rumble", 10},
 	{"Score Attack", 10},
 	{"Team Knockout", 10},
+	{"Hero Mode", 10},
+}
+
+type challenge struct {
+	guid         string
+	series       string
+	chapter      string
+	challenge    string
+	winCondition string
+}
+
+var challenges = []choose_tier{
+	{challenge{"1", "The Problem With AI", "It Started Here", "Hey! Stop That", "ScoreAscending"}, 10},
+	{challenge{"2", "The Problem With AI", "It Started Here", "Hold On Clem", "TimeDescending"}, 10},
+	{challenge{"3", "The Problem With AI", "It Started Here", "Big Mac's Mill", "TimeDescending"}, 10},
+	{challenge{"11", "The Problem With AI", "Robots Have Rights", "Returning Home", "TimeDescending"}, 10},
+	{challenge{"12", "The Problem With AI", "Robots Have Rights", "Under Warranty", "TimeDescending"}, 10},
+	{challenge{"13", "The Problem With AI", "Robots Have Rights", "How Many?", "TimeDescending"}, 10},
 }
 
 var locations = []choose_tier{
@@ -75,6 +93,16 @@ var locations = []choose_tier{
 	{"Pinch", 10},
 	{"Burping Burt", 10},
 	{"Fracking Frank's Place", 10},
+	{"Pirate Docks", 10},
+	{"Pirate Ship", 10},
+	{"Top 1", 10},
+	{"Top 2", 10},
+	{"Top 3", 10},
+	{"Island", 10},
+	{"At Sea", 10},
+	{"Local_Store", 10},
+	{"Barn", 10},
+	{"Radio Array", 10},
 }
 
 type session_context struct {
@@ -85,15 +113,15 @@ type session_context struct {
 }
 
 func (s *session_context) addEvent(eventType string, params eventPayload) {
-	s.addEventT(rand.Intn(30)+5, eventType, params)
+	s.addEventT(5+rand.Float32()*30.0, eventType, params)
 }
 
-func (s *session_context) addEventT(duration int, eventType string, params eventPayload) {
-	s.simTime += float32(duration)
+func (s *session_context) addEventT(duration float32, eventType string, params eventPayload) {
+	s.simTime += duration
 	e := event{
 		Event:     eventType,
 		UserID:    s.userId,
-		Timestamp: time.Now().Add(time.Duration(s.simTime) * time.Second),
+		Timestamp: time.Now().Add(time.Duration(s.simTime) * time.Second).UnixMilli(),
 		Params:    params,
 	}
 
@@ -207,7 +235,57 @@ func (s *session_context) sim_game_battle_arena(homeScreen string) {
 			// banana = append(banana, numBanana)
 		}
 
-		s.addEventT(rand.Intn(30)+30, "levelEnd", eventPayload{
+		s.addEventT(30+rand.Float32()*30, "levelEnd", eventPayload{
+			"player": player,
+			"score":  score,
+			// "death":  death,
+			// "banana": banana,
+		})
+	}
+
+	s.addEventT(1, "$gameEnd", nil)
+	s.addEvent("$uiScreen", eventPayload{
+		"name": homeScreen,
+	})
+}
+
+func (s *session_context) sim_challenge(homeScreen string) {
+	_type := chooseRand(battle_arena)
+	length := rand.Intn(4)*2 + 1
+
+	s.addEvent("$uiScreen", eventPayload{
+		"name": "game mode",
+	})
+	s.addEvent("$uiScreen", eventPayload{
+		"name": homeScreen,
+	})
+	s.addEvent("$gameBegin", eventPayload{
+		"type":   _type,
+		"length": length,
+	})
+
+	numPlayers := rand.Intn(2) + 2
+
+	for i := 0; i < length; i++ {
+		location := chooseRand(locations)
+		s.addEventT(3, "levelBegin", eventPayload{
+			"name": location,
+		})
+
+		player := []string{}
+		score := []int{}
+
+		for i := 0; i < numPlayers; i++ {
+			if i == 0 {
+				player = append(player, s.userId)
+			} else {
+				player = append(player, fmt.Sprintf("player%d", i+1))
+			}
+
+			score = append(score, rand.Intn(600)*50)
+		}
+
+		s.addEventT(30+rand.Float32()*30, "levelEnd", eventPayload{
 			"player": player,
 			"score":  score,
 			// "death":  death,
@@ -257,9 +335,9 @@ func (s *session_context) begin() {
 	s.userId = fmt.Sprintf("STEAM#%d", s.bucket)
 	s.sim_identify()
 	s.addEvent("$sessionBegin", eventPayload{
-		"branch":  "beta",
+		"branch":  "developer",
 		"vendor":  "steam",
-		"version": "0.1.4937",
+		"version": "0.1.6669",
 	})
 	s.addEvent("$uiScreen", eventPayload{
 		"name": "welcome",
